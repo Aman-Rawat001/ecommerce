45128865f7814016721fded4d420c61d64711277
Aman Rawat <amanrawa2@gmail.com>
2024-01-02 20:33:46 +0530

    modal created in cart page on buy now button click

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import myContext from "../../context/data/myContext";
 import Layout from "../../components/layout/Layout";
+import Modal from "../../components/modal/Modal";
 
 function Cart() {
   const context = useContext(myContext);
@@ -124,12 +125,13 @@ function Cart() {
               </div>
             </div>
             {/* <Modal  /> */}
-            <button
+            {/* <button
               type="button"
               className="w-full  bg-violet-600 py-2 text-center rounded-lg text-white font-bold "
             >
               Buy Now
-            </button>
+            </button> */}
+            <Modal />
           </div>
         </div>
       </div>
