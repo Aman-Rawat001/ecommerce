0b2b459e0d2e8564f39a7148e66f31eb2e661ed9
Aman Rawat <amanrawa2@gmail.com>
2024-01-15 16:30:25 +0530

    category slider added

--- src/pages/home/ShopByCategory/ShopByCatrgory.jsx
+++ src/pages/home/ShopByCategory/ShopByCatrgory.jsx
@@ -1,10 +1,50 @@
 import React, { useContext } from "react";
 import myContext from "../../../context/data/myContext";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ShopByCatrgory = () => {
   const context = useContext(myContext);
   const { mode } = context;
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    // cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
 
   return (
     <>
@@ -19,97 +59,109 @@ const ShopByCatrgory = () => {
             </h1>
             <div class="h-1 w-20 bg-pink-600 rounded"></div>
           </div>
-          <div className="grid lg:grid-cols-4 grid-cols-2 gap-4">
-            <Link to="/category/phone">
-              <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
-                <img
-                  className="w-48 hover:scale-110"
-                  src="https://cdn.pixabay.com/photo/2016/12/26/06/16/i-phone-1931557_1280.jpg"
-                />
-                <div>
-                  <h1
-                    className="text-center text-black font-bold  text-xl"
-                    style={{
-                      color: mode === "dark" ? "white" : "",
-                    }}
-                  >
-                    Phone
-                  </h1>
-                </div>
+          <div className="">
+            <Slider {...settings}>
+              <div className="">
+                <Link to="/category/phone">
+                  <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
+                    <img
+                      className="w-48 hover:scale-110"
+                      src="https://cdn.pixabay.com/photo/2016/12/26/06/16/i-phone-1931557_1280.jpg"
+                    />
+                    <div>
+                      <h1
+                        className="text-center text-black font-bold  text-xl"
+                        style={{
+                          color: mode === "dark" ? "white" : "",
+                        }}
+                      >
+                        Phone
+                      </h1>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/category/headphone">
-              <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
-                <img
-                  className="w-48 hover:scale-110"
-                  src="https://m.media-amazon.com/images/I/71BrtUGEvvL._SX679_.jpg"
-                />
-                <div>
-                  <h1
-                    className="text-center text-black font-bold  text-xl"
-                    style={{
-                      color: mode === "dark" ? "white" : "",
-                    }}
-                  >
-                    Headphone
-                  </h1>
-                </div>
+              <div>
+                <Link to="/category/headphone">
+                  <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
+                    <img
+                      className="w-48 hover:scale-110"
+                      src="https://m.media-amazon.com/images/I/71BrtUGEvvL._SX679_.jpg"
+                    />
+                    <div>
+                      <h1
+                        className="text-center text-black font-bold  text-xl"
+                        style={{
+                          color: mode === "dark" ? "white" : "",
+                        }}
+                      >
+                        Headphone
+                      </h1>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/category/laptop">
-              <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
-                <img
-                  className="w-48 hover:scale-110"
-                  src="https://m.media-amazon.com/images/I/41YhYmWwsNL._SX300_SY300_QL70_FMwebp_.jpg"
-                />
-                <div>
-                  <h1
-                    className="text-center text-black font-bold  text-xl"
-                    style={{
-                      color: mode === "dark" ? "white" : "",
-                    }}
-                  >
-                    Laptop
-                  </h1>
-                </div>
+              <div>
+                <Link to="/category/laptop">
+                  <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
+                    <img
+                      className="w-48 hover:scale-110"
+                      src="https://m.media-amazon.com/images/I/41YhYmWwsNL._SX300_SY300_QL70_FMwebp_.jpg"
+                    />
+                    <div>
+                      <h1
+                        className="text-center text-black font-bold  text-xl"
+                        style={{
+                          color: mode === "dark" ? "white" : "",
+                        }}
+                      >
+                        Laptop
+                      </h1>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/category/watch">
-              <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
-                <img
-                  className="w-48 hover:scale-110"
-                  src="https://m.media-amazon.com/images/I/61JtVmcxb0L._SX679_.jpg"
-                />
-                <div>
-                  <h1
-                    className="text-center text-black font-bold  text-xl"
-                    style={{
-                      color: mode === "dark" ? "white" : "",
-                    }}
-                  >
-                    Watch
-                  </h1>
-                </div>
+              <div>
+                <Link to="/category/watch">
+                  <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
+                    <img
+                      className="w-48 hover:scale-110"
+                      src="https://m.media-amazon.com/images/I/61JtVmcxb0L._SX679_.jpg"
+                    />
+                    <div>
+                      <h1
+                        className="text-center text-black font-bold  text-xl"
+                        style={{
+                          color: mode === "dark" ? "white" : "",
+                        }}
+                      >
+                        Watch
+                      </h1>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/category/earphone">
-              <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
-                <img
-                  className="w-48 hover:scale-110"
-                  src="https://m.media-amazon.com/images/I/41cTomA4G1L._SX300_SY300_QL70_FMwebp_.jpg"
-                />
-                <div>
-                  <h1
-                    className="text-center text-black font-bold  text-xl"
-                    style={{
-                      color: mode === "dark" ? "white" : "",
-                    }}
-                  >
-                    Earphones
-                  </h1>
-                </div>
+              <div>
+                <Link to="/category/earphone">
+                  <div className="shadow-lg flex items-center justify-center flex-col gap-4 py-5 hover:shadow-md border-2 rounded-lg cursor-pointer">
+                    <img
+                      className="w-48 hover:scale-110"
+                      src="https://m.media-amazon.com/images/I/41cTomA4G1L._SX300_SY300_QL70_FMwebp_.jpg"
+                    />
+                    <div>
+                      <h1
+                        className="text-center text-black font-bold  text-xl"
+                        style={{
+                          color: mode === "dark" ? "white" : "",
+                        }}
+                      >
+                        Earphones
+                      </h1>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
+            </Slider>
           </div>
         </div>
       </section>
